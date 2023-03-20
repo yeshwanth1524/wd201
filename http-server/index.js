9682c38beaa4052af324bf2f5a3bd5f0b5d7a8a4
yeshwanth1524 <127530128+yeshwanth1524@users.noreply.github.com>
2023-03-20 22:31:15 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -2,9 +2,9 @@ const http = require("http");
 const fs = require("fs");
 const argv = require("minimist")(process.argv.slice(2));
 
-const homeContent = fs.readFileSync("./home.html", "utf-8");
-const projectContent = fs.readFileSync("./project.html", "utf-8");
-const registrationContent = fs.readFileSync("./registration.html", "utf-8");
+const homeContent = fs.readFileSync("home.html", "utf-8");
+const projectContent = fs.readFileSync("project.html", "utf-8");
+const registrationContent = fs.readFileSync("registration.html", "utf-8");
 
 const server = http.createServer((request, response) => {
   const url = request.url;
@@ -12,10 +12,10 @@ const server = http.createServer((request, response) => {
   response.setHeader("Content-Type", "text/html");
 
   switch (url) {
-    case "/project.html":
+    case "/project":
       response.write(projectContent);
       break;
-    case "/registration.html":
+    case "/registration":
       response.write(registrationContent);
       break;
     default:
