8502985853240942cba423b169554c36cbb3c183
yeshwanth1524 <127530128+yeshwanth1524@users.noreply.github.com>
2023-03-20 18:20:34 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,51 +1,33 @@
 const http = require("http");
 const fs = require("fs");
-const args = process.argv.slice(2);
-const portIndex = args.findIndex((arg) => arg === "--port");
+const argv = require("minimist")(process.argv.slice(2));
 
-let contentOfHome = "";
-let contentOfProject = "";
-let contentOfRegistration = "";
-
-fs.readFile("home.html", (err, home) => {
-  if (err) {
-    throw err;
-  }
-  contentOfHome = home;
-});
-
-fs.readFile("project.html", (err, project) => {
-  if (err) {
-    throw err;
-  }
-  contentOfProject = project;
-});
-
-fs.readFile("registration.html", (err, registration) => {
-  if (err) {
-    throw err;
-  }
-  contentOfRegistration = registration;
-});
-
-const port = portIndex !== -1 ? args[portIndex + 1] : 3000;
+const homeContent = fs.readFileSync("./home.html", "utf-8");
+const projectContent = fs.readFileSync("./project.html", "utf-8");
+const registrationContent = fs.readFileSync("./registration.html", "utf-8");
 
 const server = http.createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "./project.html":
-        response.write(contentOfProject);
-        response.end();
-        break;
-      case "./registration.html":
-        response.write(contentOfRegistration);
-        response.end();
-        break;
-      default:
-        response.write(contentOfHome);
-        response.end();
-        break;
-    }
-  })
-server.listen(port);
+  const url = request.url;
+
+  response.setHeader("Content-Type", "text/html");
+
+  switch (url) {
+    case "/project.html":
+      response.write(projectContent);
+      break;
+    case "/registration.html":
+      response.write(registrationContent);
+      break;
+    default:
+      response.write(homeContent);
+      break;
+  }
+
+  response.end();
+});
+
+const port = argv.port || 3000;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
